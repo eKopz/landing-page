f3ced275a5bb46165a1a72a7b3abbb977edf3bff
Sayang Kamil5b <79760043+kamil5b@users.noreply.github.com>
2021-07-15 09:16:04 +0700

    update landing page
    -tampilan feedback berubah dari form jd button
    -add fetching.js : form harusnya uda bisa di post ke api (untested)
    -logo google play diganti jadi button biasa
    -setiap element footer di kasih margin kiri biar ga rapet bgt
    -responsibilitas footer berubah dari medium menjadi large
    - added observer buat feedback
    -Pricing berubah menjadi demo

--- DW/js/fetching.js
+++ DW/js/fetching.js
@@ -1,11 +1,9 @@
 
-const urlapi = "https://api.ekopz.id/api/visit";
-
-var visited = 0
-
+const urlget = "https://api.ekopz.id/api/visit";
+const urlpost = "http://localhost:8080/koperasi"
 window.onload = function() {
 	
-    $.getJSON(urlapi, function(data) {
+    $.getJSON(urlget, function(data) {
     	visited = data.data.visit;
 		pegguna_aktif = data.data.pengguna;
 		ko_perasi = data.data.koperasi;
@@ -16,4 +14,43 @@ window.onload = function() {
 		$("#wilayah").html(wil_ayah);
 		
     });
+	
 }
+
+/*
+const form = document.getElementById("formkoperasi")
+
+form.addEventListener('submit', function(e){
+	
+	e.preventDefault()
+
+	let formData = new FormData(this)
+	let data = JSON.stringify(Object.fromEntries(formData))
+	console.log(data)
+	fetch(urlpost, {
+	  method: 'POST', // or 'PUT'
+		mode:'no-cors',
+	  headers: {
+		'Content-Type': 'application/json',
+	  },
+	  body: data,
+	})
+	.then(response => response.json())
+	.then(data => {
+	  console.log('Success:', data);
+	})
+	.catch((error) => {
+	  console.error('Error:', error);
+	});
+	/*fetch(urlpost,{
+		method:'post',
+		body: JSON.stringify(Object.fromEntries(formData));
+	}).then(function(res){
+		return response.text();
+	}).then(function(text){
+		console.log(text)
+	}).catch(function(error){
+		console.error(error)
+	})
+
+})*/
